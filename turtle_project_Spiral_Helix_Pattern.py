4e30bcc08a5b1a5ce12bde7cbe5095a8cf8bbf24
HarshChhikara <harshchhikara1993@gmail.com>
2020-10-02 16:17:30 +0530

    Turtle project added

---
+++ turtle_project_Spiral_Helix_Pattern.py
@@ -0,0 +1,14 @@
+#python program to draw
+#Spiral Helix pattern
+#Using turtle programming
+
+import turtle 
+loadWindow = turtle.Screen() 
+turtle.speed(2) 
+  
+for i in range(100): 
+    turtle.circle(5*i) 
+    turtle.circle(-5*i) 
+    turtle.left(i) 
+  
+turtle.exitonclick() 
